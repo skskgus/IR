4ef550a36dbfdcc5c0be29cc5305fc1dd5c93bd1
skskgus1234@naver.com <>
2024-11-15 20:37:29 +0900

    add: landing page

--- securitywavefront/src/App.tsx
+++ securitywavefront/src/App.tsx
@@ -1,18 +1,19 @@
-// src/App.tsx
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import LandingPage from "./pages/LandingPage";
 import UploadPage from "./pages/UploadPage";
-import ResultPage from "./pages/ResultPage";
+import ResultPage from "./pages/ResultPage"; // ResultPage를 임포트
 
-function App() {
+const App: React.FC = () => {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<UploadPage />} />
+        <Route path="/" element={<LandingPage />} />
+        <Route path="/upload" element={<UploadPage />} />
         <Route path="/result" element={<ResultPage />} />
       </Routes>
     </Router>
   );
-}
+};
 
 export default App;
